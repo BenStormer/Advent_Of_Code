d73a6596b78496d0614b442e5955605c050afaa9
Benjamin Stormer <benjaminstormer1@gmail.com>
2025-12-01 12:18:49 -0600

    Create plan

--- 2025/aoc_problems/src/bin/1-1.rs
+++ 2025/aoc_problems/src/bin/1-1.rs
@@ -1,3 +1,19 @@
+use aoc_problems::read_lines;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("../inputs/1.txt") {
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line);
+        }
+    }
 }
+
+/*
+ * Plan:
+ * 1. Keep counter of number of times we end on 0
+ * 2. Keep tracker of current value
+ * 3. Custom functions for L + R where wraparound is handled properly
+ *   - L: subtract values and wraparound where 0 -> 99
+ *   - R: add values and wraparound where 99 -> 0
+ * 4. Return final count of times we end on 0
+ */
