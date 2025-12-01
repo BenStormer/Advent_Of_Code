545936ec81a08cc7100d4a6e9fe0dc4348e352a4
Benjamin Stormer <benjaminstormer1@gmail.com>
2025-12-01 13:18:49 -0600

    Complete Day 1, problem 1

--- 2025/aoc_problems/src/bin/1-1.rs
+++ 2025/aoc_problems/src/bin/1-1.rs
@@ -1,19 +1,34 @@
 use aoc_problems::read_lines;
 
 fn main() {
+    let mut count = 0;
+    let mut curr = 50;
     if let Ok(lines) = read_lines("../inputs/1.txt") {
         for line in lines.map_while(Result::ok) {
-            println!("{}", line);
+            if let Some(turn_value) = get_turn_value(&line) {
+                // https://stackoverflow.com/questions/31210357/is-there-a-modulus-not-remainder-function-operation
+                curr = (curr + turn_value).rem_euclid(100); // wrap around 0-99 range,
+                if curr == 0 {
+                    count += 1;
+                }
+                println!("Turn value: {} Current value: {}", turn_value, curr);
+            }
         }
     }
+    println!("Final Count: {}", count)
 }
 
-/*
- * Plan:
- * 1. Keep counter of number of times we end on 0
- * 2. Keep tracker of current value
- * 3. Custom functions for L + R where wraparound is handled properly
- *   - L: subtract values and wraparound where 0 -> 99
- *   - R: add values and wraparound where 99 -> 0
- * 4. Return final count of times we end on 0
- */
+fn get_turn_value(line: &str) -> Option<i32> {
+    let mut chars = line.chars();
+    match chars.next() {
+        Some(direction) => {
+            let magnitude: Option<i32> = chars.as_str().parse().ok();
+            match direction {
+                'L' => magnitude.map(|m| -1 * m),
+                'R' => magnitude,
+                _ => None,
+            }
+        }
+        None => None,
+    }
+}
