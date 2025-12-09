e2bdda8e2766c05db78c776cf93c20f486ab8f11
Benjamin Stormer <benjaminstormer1@gmail.com>
2025-12-08 21:21:12 -0600

    Complete Day 8, problem 2

--- 2025/aoc_problems/src/bin/8-2.rs
+++ 2025/aoc_problems/src/bin/8-2.rs
@@ -1,6 +1,5 @@
 use aoc_problems::read_lines;
-use std::cmp::Reverse;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
 /*
  * Algorithm:
@@ -11,24 +10,19 @@ use std::collections::{BTreeMap, HashMap};
  *    - Adjacency matrix step could be skipped, but I am assuming it will be
  *      useful for part 2
  * 3. Create a HashMap of groups (coordinate, groupNumber)
- *    - For the first NUM_CONNECTIONS entries in the BTree HashMap, look up
- *      coordinates in the group HashMap.
- *       - If one of the coordinates is already in a group, add the other one
- *         to the same group
+ *    - For ALL entries in the BTree HashMap, look up coordinates in the group
+ *      HashMap.
+ *       - If one of the coordinates is already in a group, add the other one to
+ *         the same group
  *       - If neither coordinate is in a group, create a new group with them
- * 4. Repeat until all coordinates are sorted in groups
- * 5. Count the number of coordinates in each group and store in new BTree
- *    Set (since it is sorted)
- * 6. Multiply the # of coordinates in the 3 largest groups
+ * 4. Repeat until all coordinates are sorted in groups and groups get combined into one
+ * 5. Multiply the "x" value of the two coordinates that cause this
  */
-
-const NUM_CONNECTIONS: usize = 1000;
-const TOP_N_MEMBER_COUNTS: usize = 3;
 
 type Coordinate = (i64, i64, i64);
 
 fn main() {
-    let mut coordinates: Vec<(i64, i64, i64)> = Vec::new();
+    let mut all_coordinates: Vec<(i64, i64, i64)> = Vec::new();
     if let Ok(lines) = read_lines("./inputs/8.txt") {
         for line in lines.map_while(Result::ok) {
             let coordinate_vector: Vec<i64> = line
@@ -40,15 +34,15 @@ fn main() {
                 coordinate_vector[1],
                 coordinate_vector[2],
             );
-            coordinates.push(coordinate);
+            all_coordinates.push(coordinate);
         }
     }
 
     let mut distance_hashmap: BTreeMap<i64, (Coordinate, Coordinate)> = BTreeMap::new();
-    for coordinate_1_index in 0..coordinates.len() {
-        for coordinate_2_index in (coordinate_1_index + 1)..coordinates.len() {
-            let coordinate_1 = coordinates[coordinate_1_index];
-            let coordinate_2 = coordinates[coordinate_2_index];
+    for coordinate_1_index in 0..all_coordinates.len() {
+        for coordinate_2_index in (coordinate_1_index + 1)..all_coordinates.len() {
+            let coordinate_1 = all_coordinates[coordinate_1_index];
+            let coordinate_2 = all_coordinates[coordinate_2_index];
             let distance = calculate_distance(coordinate_1, coordinate_2);
 
             // Construct sorted Hashmap (distance as key to sort by it)
@@ -58,24 +52,33 @@ fn main() {
 
     // Calculate group pairings for all coordinates (coord: group_number)
     let mut group_hashmap: HashMap<Coordinate, i64> = HashMap::new();
+    let mut unique_groups: HashSet<i64> = HashSet::new();
     let mut group_count = 0;
-    for (_distance, coordinates) in distance_hashmap.iter().take(NUM_CONNECTIONS) {
+    let mut last_coordinates: (Coordinate, Coordinate) = ((-1, -1, -1), (-1, -1, -1));
+    for (_distance, coordinates) in distance_hashmap {
+        // Check if we have combined all circuits
+        if unique_groups.len() == 1 && group_hashmap.len() >= all_coordinates.len() {
+            break;
+        }
+
         let coordinate_1 = coordinates.0;
         let coordinate_2 = coordinates.1;
+        last_coordinates = (coordinate_1, coordinate_2);
 
         // Check if coordinates are already in group_hashmap
         if group_hashmap.contains_key(&coordinate_1) && group_hashmap.contains_key(&coordinate_2) {
-            let coordinate_1_group = group_hashmap
+            let &coordinate_1_group = group_hashmap
                 .get(&coordinate_1)
                 .expect("Invalid group number");
-            let coordinate_2_group = group_hashmap
+            let &coordinate_2_group = group_hashmap
                 .get(&coordinate_2)
                 .expect("Invalid group number");
             if coordinate_1_group == coordinate_2_group {
                 continue;
             } else {
                 // Combine groups for group 1 and group 2
-                combine_groups(*coordinate_1_group, *coordinate_2_group, &mut group_hashmap);
+                combine_groups(coordinate_1_group, coordinate_2_group, &mut group_hashmap);
+                unique_groups.remove(&coordinate_2_group);
             }
         } else if group_hashmap.contains_key(&coordinate_1) {
             let coordinate_1_group = group_hashmap
@@ -91,28 +94,12 @@ fn main() {
             group_count += 1;
             group_hashmap.insert(coordinate_1, group_count);
             group_hashmap.insert(coordinate_2, group_count);
+            unique_groups.insert(group_count);
         }
     }
 
-    // Count the number of members for each group
-    let mut group_member_counts: BTreeMap<Reverse<i64>, i64> = BTreeMap::new();
-    for group_number in 1..=group_count {
-        let member_count = group_hashmap
-            .values()
-            .filter(|&&val| val == group_number)
-            .count() as i64;
-        group_member_counts.insert(Reverse(member_count), group_number);
-    }
-
-    // Get top 3 member_counts and multiply
-    let top_n_member_counts: Vec<&i64> = group_member_counts
-        .iter()
-        .take(TOP_N_MEMBER_COUNTS)
-        .map(|(Reverse(member_count), _group_number)| member_count)
-        .collect();
-    println!("Top N Member Counts: {:?}", top_n_member_counts);
-
-    let final_answer: i64 = top_n_member_counts.iter().map(|&&x| x).product();
+    // Multiply the "x" values of the coordinates that caused the last groups to combine
+    let final_answer: i64 = last_coordinates.0.0 * last_coordinates.1.0;
     println!("Final Answer: {}", final_answer);
 }
 
